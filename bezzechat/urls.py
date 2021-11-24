abdb2ef36a4b425c207b17efa5e2cee936ba976e
Haris Gušić <harisgusic.dev@gmail.com>
2021-11-24 19:58:18 +0100

    Add register page frontend
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- bezzechat/urls.py
+++ bezzechat/urls.py
@@ -21,5 +21,7 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
+    path("register/", views.register, name="register"),
+    path("login/", views.login, name="register"),
     path("chat/", views.chat, name="chat"),
 ]
--- bezzechat/views.py
+++ bezzechat/views.py
@@ -7,6 +7,16 @@ def index(request):
     return render(request, "index.html", {})
 
 
+def register(request):
+    """New user registration page."""
+    return render(request, "register.html", {})
+
+
+def login(request):
+    """Login page for existing users."""
+    return render(request, "login.html", {})
+
+
 def chat(request):
     """The page with chat and sidebar."""
     return render(request, "chat.html", {})
