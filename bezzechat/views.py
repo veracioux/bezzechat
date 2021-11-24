d944f0ac417e4a7284af6e0fd13d596a0065a67f
Haris Gušić <harisgusic.dev@gmail.com>
2021-11-24 13:50:38 +0100

    Add basic chat interface (frontend)
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- bezzechat/views.py
+++ bezzechat/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     """The root page."""
     return render(request, "index.html", {})
+
+
+def chat(request):
+    """The page with chat and sidebar."""
+    return render(request, "chat.html", {})
